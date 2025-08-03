f10eb43b7c6160fc8609765f1b8fa4af3ddf4314
AgungDH <agungdh@live.com>
2025-08-03 08:45:31 +0700

    feat: expose list of all projects via GraphQL query
    - Added `projects` query to `ProjectController` to return all projects
    - Updated GraphQL schema:
      - Added `projects: [Project]` to `Query` type
      - Changed `id` field type from `String` to `ID!` in `Project`
      - Renamed `createdAt` to `releaseDate` in `Project` type to match Java entity

--- src/main/java/id/my/agungdh/api/controller/ProjectController.java
+++ src/main/java/id/my/agungdh/api/controller/ProjectController.java
@@ -7,12 +7,18 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
 import java.util.UUID;
 
 @Controller
 @AllArgsConstructor
 public class ProjectController {
     private ProjectService projectService;
+
+    @QueryMapping
+    public List<Project> projects() {
+        return projectService.findAll();
+    }
 
     @QueryMapping
     public Project projectById(@Argument UUID id) {
